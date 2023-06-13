082c03bae73d824c7cd4a11a2d25695b9b1575e7
AliceMochi <118159857+AliceMochi@users.noreply.github.com>
2023-06-13 15:06:58 +0700

    Add files via upload

---
+++ leckerscapstone/recommendation_api.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from recommendation_engine import rekomendasi_resep #Importing engine function
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+
+app.config['JSON_SORT_KEYS'] = False
+
+dataset_resep = pd.read_csv('dataset_all.csv')
+
+cosine_sim_df = pd.read_csv('cosine_sim_df.csv')
+
+
+#API endpoint
+@app.route('/api', methods=['POST'])
+def process_request():
+
+
+    #Parse received JSON request
+    user_input = request.get_json()
+
+    #Extract show title
+    Bahan = user_input['Ingredient']
+
+    #Call recommendation engine
+    rekomendasi_resep_dict = rekomendasi_resep(Bahan, cosine_sim_df, dataset_resep)
+
+    return jsonify(rekomendasi_resep_dict)
+
+
+if __name__ == '__main__':
+
+    app.run()
